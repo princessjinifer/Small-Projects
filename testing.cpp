ed3bc3ee4c292a8cdfe4a34284cdf86604a7a94c
Micah Butler <jgordon24@zcat.com>
2012-04-01 09:09:42 -0500

    added testing; changed averages;

---
+++ testing.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+// Used for input of two names
+char firstName[30];
+char lastName[30];
+char askFirstName[] = "First name: ";
+char askLastName[] = "Last name: ";
+
+// Used for adding together the two names
+string namesTogether;
+
+int main() {
+//	Input first name
+    cout << askFirstName;
+	cin >> firstName;
+//  Input last name
+    cout << askLastName;
+	cin >> lastName;
+
+//	Combine the first and last name together
+    namesTogether = firstName;
+	namesTogether += lastName;
+
+//	Display resulting string
+    cout << namesTogether << "\n";
+
+    return 0;
+}
